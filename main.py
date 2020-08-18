cf1af44a79391d6c7eec0beda915b19a75261d52
john doe <scottbruceburton@gmail.com>
2020-08-18 14:05:00 -0400

    simple changes

--- main.py
+++ main.py
@@ -4,24 +4,30 @@
 from pathlib import Path
 
 print('What text file would you like to madlib?')
-inFile = Path(input())
+inFile = open(input())
 
 # TODO: Read contents of input file for keywords
 # ADJECTIVE NOUN VERB
 
 # check path validity
-if inFile.is_file():
-    inFile.open()
-    inText = inFile.read_text()
+#if inFile.is_file():
+#    inFile.open()
+#    inText = inFile.read_text()
+#    inFile.close()
 #    print(inText)
-else:
-    print('You entered an invalid File')
-    return -1
+#else:
+#    print('You entered an invalid File')
 
 switchWords = ('ADJECTIVE', 'NOUN', 'VERB')
 
+fileString = inFile.read()
+wordList = fileString.split()
+print(wordList)
+
 
 # TODO: Ask User for corresponding words input
+
+
 
 # TODO: Write to new text file.
 
