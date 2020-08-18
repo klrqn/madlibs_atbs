49e435fc9f8480517cff6d0024872d8190b05a47
john doe <scottbruceburton@gmail.com>
2020-08-18 16:21:01 -0400

    basic working

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-#! python3
-# Makes a silly madlibs
-
-from pathlib import Path
+#! python3 # Makes a silly madlibs from pathlib import Path
 
 print('What text file would you like to madlib?')
 inFile = open(input())
-
-# TODO: Read contents of input file for keywords
-# ADJECTIVE NOUN VERB
-
-# check path validity
-#if inFile.is_file():
-#    inFile.open()
-#    inText = inFile.read_text()
-#    inFile.close()
-#    print(inText)
-#else:
-#    print('You entered an invalid File')
-
-switchWords = ('ADJECTIVE', 'NOUN', 'VERB')
-
 fileString = inFile.read()
+inFile.close()
+
+# Split String into list for analysis
 wordList = fileString.split()
-print(wordList)
 
+# Find words to replace in list, replace them with user input
+for i in range(len(wordList)):
+        if wordList[i] == 'ADJECTIVE':
+            print('Give me an Adjective: ')
+            a = input()
+            wordList[i] = a
+        if wordList[i] == 'NOUN':
+            print('Give me a Noun: ')
+            n = input()
+            wordList[i] = n
+        if wordList[i] == 'VERB':
+            print('Give me a Verb: ')
+            v = input()
+            wordList[i] = v
 
-# TODO: Ask User for corresponding words input
+# print(f'Word List New = {wordList}')
 
-
-
-# TODO: Write to new text file.
-
+# Write to new text file.
+outFile = open('MaDlIbs.txt', 'w')
+madlibs = ' '.join(wordList)
+print(f'Madlibs String: {madlibs}')
+outFile.write(madlibs)
+outFile.close()
